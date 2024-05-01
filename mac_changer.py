5c5608e0b5d0713d9f35ea70f7586c5646704a36
root <tahamb05@gmail.com>
2024-05-01 03:00:39 -0400

    Comman-line arguments

--- mac_changer.py
+++ mac_changer.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 
 import subprocess
+import optparse
 
-#use "raw_input" for python2
-interface = input("Interface > ")
-new_mac = input("New MAC > ")
+parser = optparse.OptionParser()
+
+parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+
+(options, arguments) = parser.parse_args()
+
+
+interface = options.interface
+new_mac = options.new_mac
 print("[+]Changing MAC Address for " + interface + " to " + new_mac)
 
 subprocess.call(["ifconfig", interface, "down"])
