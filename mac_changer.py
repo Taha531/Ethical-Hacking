2ab6b8eb46677e140a590b46e7cc76173c441ff4
root <tahamb05@gmail.com>
2024-05-01 02:39:55 -0400

    taking user input

--- mac_changer.py
+++ mac_changer.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 
 import subprocess
-subprocess.call("ifconfig eth0 down", shell=True)
-subprocess.call("ifconfig eth0 hw ether 00:11:22:33:44:66", shell=True)
-subprocess.call("ifconfig eth0 up", shell=True)
-subprocess.call("ifconfig", shell=True)
+
+#use "raw_input" for python2
+interface = input("Interface > ")
+new_mac = input("New MAC > ")
+print("[+]Changing MAC Address for " + interface + " to " + new_mac)
+
+subprocess.call("ifconfig " + interface + " down", shell=True)
+subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+subprocess.call("ifconfig " + interface + " up", shell=True)
+
