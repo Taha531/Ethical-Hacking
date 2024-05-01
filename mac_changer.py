f7763590a4ccf7161a6a84d5349f00998e602dbe
root <tahamb05@gmail.com>
2024-05-01 02:47:08 -0400

    securing

--- mac_changer.py
+++ mac_changer.py
@@ -7,7 +7,6 @@ interface = input("Interface > ")
 new_mac = input("New MAC > ")
 print("[+]Changing MAC Address for " + interface + " to " + new_mac)
 
-subprocess.call("ifconfig " + interface + " down", shell=True)
-subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
-subprocess.call("ifconfig " + interface + " up", shell=True)
-
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
