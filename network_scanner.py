cd6c43745761477546eddd6584b77ff158756277
root <tahamb05@gmail.com>
2024-05-03 15:03:15 -0400

    Creating EP request directed to MAC asking for IP (i) Using ARP to ask who has target IP (ii) Set Destination MAC to broadcast MAC

--- network_scanner.py
+++ network_scanner.py
@@ -4,7 +4,15 @@ import scapy.all as scapy
 
 
 def scan(ip):
-    scapy.arping(ip)
+    arp_request = scapy.ARP(pdst = ip) #Creating an ARP packet to send
+    arp_request.show()
+    broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff") #Setting the destination MAC in the ethernet part
+    broadcast.show()
+    arp_request_broadcast = broadcast/arp_request #Combining both the packets
+    arp_request_broadcast.show() #Give more details about the contains of the packet
+    # scapy.ls(scapy.ARP()) --> This code is used to list all the options and their default values in scapys
+
+
 
 #IP range
-scan("192.168.29.2")
+scan("192.168.29.1/24")
