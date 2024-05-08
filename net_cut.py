05b4633ce4a56b57f2501e9e6e692264f916f688
root <tahamb05@gmail.com>
2024-05-08 13:26:24 -0400

    making a queue for incoming packets

--- net_cut.py
+++ net_cut.py
@@ -5,8 +5,9 @@ import netfilterqueue
 
 def process_packet(packet):
     print(packet)
+    packet.drop()
 
 
-queue = netfilterqueue.Netfilterqueue()
+queue = netfilterqueue.NetfilterQueue()
 queue.bind(0, process_packet)
 queue.run()
